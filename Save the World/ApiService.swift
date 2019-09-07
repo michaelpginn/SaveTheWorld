e6446b5866f3923d1aa53207df16d366b7b2f6dc
Lu Gillespie <gillespie.lucinda@gmail.com>
2019-09-07 12:23:33 -0500

    fixed api for ya

--- Save the World/ApiService.swift
+++ Save the World/ApiService.swift
@@ -11,45 +11,55 @@ import Firebase
 
 class ApiService: NSObject {
     
+    let db = Firestore.firestore()
     
-//    var username:String?
-//    var score: Int?
-//    
-//    override init(){
-//        username = UserDefaults.standard.string(forKey: "username")
-//        score = UserDefaults.standard.integer(forKey: "score")
-//    }
-//    
-//    /// Sends the score to the api for the current user
-//     func postScore(){
-//        guard let username = self.username else {return}
-//        ref.child("users").child(username).setValue(["score": score])
-//    }
-//    
-//    /// Gets an array of the latest actions for the user feed, by querying for actions matching a user's friends
-//     func getFeed()->[Action]{
-//        return []
-//    }
-//    
-//    func checkUsernameExists(username:String, completion:@escaping (Bool)->Void){
-//        ref.child("users").observeSingleEvent(of: .value, with:{ snapshot in
-//            if snapshot.hasChild(username){
-//                completion(false)
-//            }
-//        })
-//        completion(true)
-//    }
-//    
-//    /// Gets non completed tasks for a user
-//    func getTasks(completedTasks:[Task])->[Task]{
-//        return []
-//    }
-//    
-//    /// Signs up a user with a given username, returns success
-//    func signUp(username:String, completion:@escaping (Bool)->Void){
-//        checkUsernameExists(username: username) { (success) in
-//            if success{ self.ref.child("users").child(username).setValue(["username": username, "score":0])
-//            }
-//        }
-//    }
+    var username:String?
+    var score: Int?
+    
+    override init(){
+        username = UserDefaults.standard.string(forKey: "username")
+        score = UserDefaults.standard.integer(forKey: "score")
+    }
+    
+    /// Sends the score to the api for the current user
+     func postScore(){
+        guard let username = self.username else {return}
+        db.collection("users").document(username).setData(["score": score])
+    }
+    
+    
+    /// Gets an array of the latest actions for the user feed, by querying for actions matching a user's friends
+    func getFeed(completion: @escaping (QuerySnapshot?, Error?) -> Void){
+        db.collection("actions").getDocuments(completion: completion)
+    }
+    
+    func checkUsernameExists(username:String, completion:@escaping (Bool)->Void){
+        db.collection("users").whereField("username", isEqualTo: username).getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("error in getting doc: \(error)")
+            }
+            else{
+                if let querySnapshot = querySnapshot {
+                    completion(!querySnapshot.isEmpty)
+                }
+            }
+        }
+    }
+    
+    /// Gets non completed tasks for a user
+    func getTasks(completion: @escaping (QuerySnapshot?, Error?) -> Void){
+        db.collection("tasks")
+        .order(by: "score")
+        .limit(to: 10)
+        .getDocuments(completion: completion)
+    }
+
+    /// Signs up a user with a given username, returns success
+    func signUp(username:String, completion:@escaping (Bool)->Void){
+        checkUsernameExists(username: username) { (exists) in
+            if !exists{
+                self.db.collection("users").document(username).setData(["username": username, "score":0])
+            }
+        }
+    }
 }
