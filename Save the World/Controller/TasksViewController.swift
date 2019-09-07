d2ac91addcad4d90dee711314b95557de9770e8b
Michael Ginn <milespergallon271@gmail.com>
2019-09-07 13:54:15 -0400

    fuck

---
+++ Save the World/Controller/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksViewController.swift
+//  Save the World
+//
+//  Created by Michael Ginn on 9/7/19.
+//  Copyright © 2019 Michael Ginn. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDataSource {
+    var tasks:[Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
