53b1c0f2b2b1b72332e485f756ebb0187463c1e1
Michael Ginn <milespergallon271@gmail.com>
2019-09-06 20:14:35 -0400

    added view controller class

---
+++ Save the World/Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Save the World
+//
+//  Created by Michael Ginn on 9/6/19.
+//  Copyright © 2019 Michael Ginn. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
