50fad0ac143a288a8791f942ce1f1754d52214bc
Michael Ginn <milespergallon271@gmail.com>
2019-09-07 22:40:29 -0400

    fixed long press

--- Save the World/View/TaskCellVIew.swift
+++ Save the World/View/TaskCellVIew.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol TaskCellDelegate{
+    func completeTask(task:Task)
+}
+
 class TaskCellView: UIView {
     @IBOutlet weak var titleLabel:UILabel!
     @IBOutlet weak var descriptionLabel:UILabel!
@@ -17,19 +21,19 @@ class TaskCellView: UIView {
     var animator:UIViewPropertyAnimator?
     var bar:UIView?
     
+    var delegate:TaskCellDelegate?
+    var task:Task?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let subview = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 9))
+        let subview = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 6))
         subview.backgroundColor = UIColor(displayP3Red: 107/255.0, green: 147/255.0, blue: 214/255.0, alpha: 1.0)
         self.progressBar.addSubview(subview)
         print(progressBar.frame)
@@ -42,12 +46,15 @@ class TaskCellView: UIView {
             print("done")
         }
         self.animator = UIViewPropertyAnimator(duration: 1.5, curve: .linear, animations: {
-            self.bar?.frame = CGRect(x: 0, y: 0, width: self.progressBar.frame.width, height: 9)
+            self.bar?.frame = CGRect(x: 0, y: 0, width: self.progressBar.frame.width, height: 6)
         })
         self.animator?.addCompletion({ (position) in
             self.bar?.removeFromSuperview()
             self.bar = nil
             //actually run the event
+            if self.delegate != nil, let task = self.task{
+                self.delegate?.completeTask(task: task)
+            }
         })
         self.animator?.startAnimation()
     }
