0a9ed26c688ef2f1c4a7fea659208c06791e5594
Michael Ginn <milespergallon271@gmail.com>
2019-09-07 20:58:32 -0400

    improved interfcea

---
+++ Save the World/View/TaskCellVIew.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCellVIew.swift
+//  Save the World
+//
+//  Created by Michael Ginn on 9/7/19.
+//  Copyright © 2019 Michael Ginn. All rights reserved.
+//
+
+import UIKit
+
+class TaskCellVIew: UIView {
+
+    
+
+}
